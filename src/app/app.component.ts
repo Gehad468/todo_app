4516f9117303a5ae2c3fea942229ab3f0f08e30f
gehad <gehadmarawan7@gmail.com>
2024-12-30 13:02:05 +0200

    to do app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WrapperComComponent } from './wrapper-com/wrapper-com.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,WrapperComComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
